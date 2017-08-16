c12fc0307dd9d16838e2253c040f42dd829c8aa1
alex <mountain@nesumg.com>
2017-08-16 14:04:23 +0800

    增加通用异常判断

---
+++ MyApplication/app/src/main/java/cn/lxt/nucleusdemo/retrofit/EmptyThrowableConsumer.java
@@ -0,0 +1,34 @@
+package cn.lxt.nucleusdemo.retrofit;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
+
+/**
+ * Created by CN-11 on 2017/7/7.
+ */
+
+public class EmptyThrowableConsumer implements Consumer<Throwable> {
+
+    private Context context;
+
+    public EmptyThrowableConsumer() {
+    }
+
+
+    public EmptyThrowableConsumer(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void accept(@NonNull Throwable throwable) throws Exception {
+//        Log.e("EmptyThrowableConsumer", "throwable=== >" + throwable.toString());
+        if(context != null){
+            Toast.makeText(context, "网络异常", Toast.LENGTH_LONG, Gravity.CENTER).show();
+        }
+    }
+}
